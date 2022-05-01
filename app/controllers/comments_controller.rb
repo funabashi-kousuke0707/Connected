8125201ccd3d1c0dc162fa01f0de4764fed607a0
FunabashiKousuke <funabashi.kousuke.0707@gmail.com>
2022-05-01 10:14:13 +0900

    postモデルとcommentモデルを紐付けて投稿することができる
    commentテーブルへ投稿するとコメント対象の投稿物(post_id)の情報も保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,13 @@
 class CommentsController < ApplicationController
-  
-  def new
-    @comment = Comment.new(comment_params)
-    binding.pry
-  end
 
   def create
+    @post = Post.find(params[:post_id])
     @comment = Comment.create(comment_params)
+    binding.pry
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:name,:text).merge(:post_id post.id )
+      params.require(:comment).permit(:name,:text).merge(post_id: @post.id)
     end
 end

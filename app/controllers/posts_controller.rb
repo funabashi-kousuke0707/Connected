e8d1d7947f101710fdf0e41ae4f9c28385f7b0e1
FunabashiKousuke <funabashi.kousuke.0707@gmail.com>
2022-05-13 17:06:59 +0900

    @postに紐づいたコメントのみ格納

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.all
+    @comments = @post.comment
   end
 
   private

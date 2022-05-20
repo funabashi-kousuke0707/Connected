e2de98a06202df8e517f29ccdc17ab6c88213cb6
FunabashiKousuke <funabashi.kousuke.0707@gmail.com>
2022-05-20 22:54:08 +0900

    Postに紐づいたゲストユーザーのコメント(Comment)とPostに紐づいたログインユーザーのコメント(UserComment)のオブジェクトを投稿順に並び替えて格納

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comment = Comment.new
     @user_comment = UserComment.new
-    @comments = @post.comment
+    # Postに紐づいたゲストユーザーのコメント(Comment)とPostに紐づいたログインユーザーのコメント(UserComment)のオブジェクトを投稿順に並び替えて格納
+    @mixed = (@post.comment + @post.user_comment).sort_by {|record| record.created_at}.reverse!
   end
 
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_many :comment,dependent: :destroy
+  has_many :user_comment,dependent: :destroy
 end

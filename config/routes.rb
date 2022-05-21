f0012dad440c3888bcb24bdc193d8b9c99d80ee2
FunabashiKousuke <funabashi.kousuke.0707@gmail.com>
2022-05-22 08:02:26 +0900

    guestcomment_to_userpostsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
-  resources :user_posts
+  resources :user_posts do
+    resources :guestcomment_to_userposts, only:[:create]
+  end
   resources :posts do
     resources :comments, only:[:create]
     resources :user_comments,only:[:create]

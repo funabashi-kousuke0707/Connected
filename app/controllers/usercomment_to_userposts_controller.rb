ef44307297764cc3a707ba7766399b3f7875473a
FunabashiKousuke <funabashi.kousuke.0707@gmail.com>
2022-05-22 22:48:50 +0900

    UsercommentToUserpostにuser_id,user_post_idを紐付けて投稿できるようにしました

--- app/controllers/usercomment_to_userposts_controller.rb
+++ app/controllers/usercomment_to_userposts_controller.rb
@@ -1,2 +1,12 @@
 class UsercommentToUserpostsController < ApplicationController
+  def create
+    @user_post = UserPost.find(params[:user_post_id])
+    @usercomment_to_userpost = UsercommentToUserpost.create(usercomment_to_userpost_params)
+    redirect_to user_post_path(@usercomment_to_userpost.user_post)
+  end
+  
+  private 
+    def usercomment_to_userpost_params
+      params.require(:usercomment_to_userpost).permit(:name,:text).merge(user_post_id: @user_post.id,user_id: current_user.id)
+    end
 end
